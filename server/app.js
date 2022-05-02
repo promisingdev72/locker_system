291cd015b0b233ef4a7dc85a2b37ac99f9a87a8d
promisingdev72 <promising.dev72@gmail.com>
2022-05-02 14:19:13 +0900

    update

--- server/app.js
+++ server/app.js
@@ -1,6 +1,26 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
-app.use((request, response) => {
-  response.json({ message: 'Hey! This is your server response!' });
+const mysql = require('mysql');
+// const path = require('path');
+const bodyParser = require('body-parser');
+// const publicPath = path.join(__dirname, '../build');
+
+// app.use(express.static(publicPath));
+
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(publicPath, 'index.html'));
+// });
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.post('/api/account/login', function (req, res) {
+  const result = {
+    email: req.body.email,
+    password: req.body.password,
+  };
+  console.log(result);
+  res.send();
 });
+
 module.exports = app;
